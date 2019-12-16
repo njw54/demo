02cafb3da7dc5f81a394a275cc0071153995b5d8
codeinspectordemo <julien@gunnm.org>
2019-12-15 21:46:41 -0800

    Address comments

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-
-
-
-
-def sum(a, b):
-    return a + b
+def sumTwoTerms(firstTerm, secondTerm):
+    """
+    Make a sum for two terms.
+    :param firstTerm: the first term to sum
+    :param secondTerm: the second term to sum
+    :return: sun of both terms
+    """
+    return firstTerm + secondTerm
 
 
 print("2 + 2 = {0}".format(sum(2, 2)))
