993d1c5b2a6055d713f57c071ca408150f23b7d5
codeinspectordemo <julien@gunnm.org>
2019-12-15 22:01:16 -0800

    update naming

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def sumTwoTerms(firstTerm, secondTerm):
+def sum_two_terms(first_term, second_term):
     """
     Make a sum for two terms.
     :param firstTerm: the first term to sum
     :param secondTerm: the second term to sum
     :return: sun of both terms
     """
-    return firstTerm + secondTerm
+    return first_term + second_term
 
 
-print("2 + 2 = {0}".format(sumTwoTerms(2, 2)))
+print("2 + 2 = {0}".format(sum_two_terms(2, 2)))
