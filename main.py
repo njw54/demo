c0bbd8a5f1f7fec09c9c7f8e1f1fb2bc3cffb56a
codeinspectordemo <julien@gunnm.org>
2019-12-18 21:31:47 -0800

    fix issues

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def sumTwoTerms(term1, term2):
+""" My first Python module """
+
+
+def sum_two_terms(term1, term2):
+    """
+    Add two terms
+    :param term1: the first term
+    :param term2: the second term
+    :return:
+    """
     return term1 + term2
 
 
-print("2 + 2 = {0}".format(sumTwoTerms(2, 2)))
+print("2 + 2 = {0}".format(sum_two_terms(2, 2)))
